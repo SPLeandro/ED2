515824bb122f14b7461e9deb3fbf077e7909bfb9
SPLeandro <58618092+SPLeandro@users.noreply.github.com>
2020-11-30 23:19:59 -0300

    Add files via upload

---
+++ Atividade01/Atividade01/Venda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade01
+{
+    class Venda
+    {
+        private int qtde;
+        private double valor;
+
+        public Venda (int qtde, double valor)
+        {
+            this.qtde = qtde;
+            this.valor = valor;
+        }
+
+        public Venda() : this(0, 0.0)
+        {
+        }
+
+        public int Qtde
+        {
+            get { return qtde; }
+            set { qtde = value; }
+        }
+
+        public double Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public double valorMedio()
+        {
+            return (valor / qtde);
+        }
+    }
+}
---
+++ Atividade01/Atividade01/Vendedores.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade01
+{
+    class Vendedores
+    {
+        private Vendedor[] osVendedores;
+        private int max;
+        private int qtde = 0;
+
+        public Vendedores(int max)
+        {
+            this.max = max;
+            this.qtde = 0;
+            this.osVendedores = new Vendedor[this.max];
+
+            for (int i = 0; i < this.max; ++i)
+            {
+                this.osVendedores[i] = new Vendedor(-1, "...", 0.0);
+            }
+        }
+
+        public int Qtde
+        {
+            get { return qtde; }
+            set { qtde = value; }
+        }
+
+        public Vendedor[] OsVendedores
+        {
+            get { return osVendedores; }
+            set { osVendedores = value; }
+        }
+
+
+        public bool addVendedor(Vendedor v)
+        {
+            bool podeAdicionar = (this.qtde < this.max);
+            if (podeAdicionar)
+            {
+                this.osVendedores[this.qtde] = v;
+                this.qtde++;
+            }
+            return podeAdicionar;
+        }
+
+
+        public bool delVendedor(Vendedor vendedor)
+        {
+            bool temCoisa = false;
+
+            foreach (Vendedor v in this.osVendedores)
+            {
+                if (v.Equals(vendedor))
+                {
+                    v.Id = -1;
+                    v.Nome = "...";
+                    v.PercComissao = 0.0;
+                    v.AsVendas = new Venda[31];
+                    temCoisa = true;
+                }
+            }
+            return temCoisa;
+        }
+
+
+        public Vendedor searchVendedor(string nome)
+        {
+            Vendedor vendedorEncontrado = new Vendedor(1, "", 0.2);
+
+            int i = 0;
+
+            while (i < this.max && !this.osVendedores[i].Nome.Equals(nome))
+            {
+                i++;
+            }
+            if (i < this.max)
+            {
+                vendedorEncontrado = this.osVendedores[i];
+            }
+
+            return vendedorEncontrado;
+ 
+        }
+
+        public double valorVendas()
+        {
+            double total = 0.0;
+
+            foreach (Vendedor v in this.osVendedores)
+            {
+                total += v.valorVendas();
+            }
+
+            return total;
+        }
+
+        public double valorComissao()
+        {
+            double total = 0.0;
+
+            foreach (Vendedor v in this.osVendedores)
+            {
+                total += v.valorComissao();
+            }
+
+            return total;
+        }
+    }
+}
